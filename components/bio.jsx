3ad1ffadc893d7f53789d3e66c966a1b07940295
developer-gabe <gsousa@ampagency.com>
2023-02-15 18:04:23 -0700

    Convert Bio component from Class to Functional

--- components/bio.jsx
+++ components/bio.jsx
@@ -1,92 +1,94 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Bio extends Component {
+const Bio = ({ userName, userImage, userTitle, userBio }) => {
+  const defaultProps = {
+    userName: "FirstName LastName",
+    userImage: "/images/gabe.jpg",
+    userTitle: "Title",
+    userBio:
+      "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  };
 
-		static defaultProps = {
-			userName: "FirstName LastName",
-			userImage: "/images/gabe.jpg",
-			userTitle: "Title",
-			userBio: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-		};
+  userName = userName || defaultProps.userName;
+  userImage = userImage || defaultProps.userImage;
+  userTitle = userTitle || defaultProps.userTitle;
+  userBio = userBio || defaultProps.userBio;
 
-		render() {
-				return (
-						<div className="bio-grid">
-							<div className="bio-image" style={{ backgroundImage: `url(${this.props.userImage})` }}> 
-							</div>
-							<div className="bio-info">
-								<h1>{this.props.userName}</h1>
-								<h2>{this.props.userTitle}</h2>
-								<p>{this.props.userBio}</p>
-							</div>
-						
-								<style jsx global>{`
-										.bio-grid {
-											display: grid;
-											grid-template-columns: 1fr 1fr;
-											align-items: center;
-											height: 500px;
-											background-color: #16363a;
-											box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0, 0, 0, 0.295);
-										}
+  return (
+    <div className="bio-grid">
+      <div className="bio-image" style={{ backgroundImage: `url(${userImage})` }}>
+      </div>
+      <div className="bio-info">
+        <h1>{userName}</h1>
+        <h2>{userTitle}</h2>
+        <p>{userBio}</p>
+      </div>
 
-										.bio-info {
-											font-family: 'Roboto', sans-serif;
-											padding: 1rem;
-										}
+      <style jsx global>{`
+        .bio-grid {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          align-items: center;
+          height: 500px;
+          background-color: #16363a;
+          box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),
+            0 10px 10px rgba(0, 0, 0, 0.295);
+        }
 
-										.bio-info h1 {
-											font-size: 2rem;
-											color: #D7A08B;
-											margin-bottom: .35rem;
-											text-transform: uppercase;
-										}
+        .bio-info {
+          font-family: "Roboto", sans-serif;
+          padding: 1rem;
+        }
 
-										.bio-info h2 {
-											margin-top: 0;
-											margin-left: .15rem;
-											margin: 0 .15rem 1rem .15rem;
-											font-size: .9rem;
-											color: #fff;
-											font-weight: 300;
-											text-transform: uppercase;
-											letter-spacing: 2px;
-										}
+        .bio-info h1 {
+          font-size: 2rem;
+          color: #d7a08b;
+          margin-bottom: 0.35rem;
+          text-transform: uppercase;
+        }
 
-										.bio-info p {
-											color: #D7A08B;
-											line-height: 2;
-										}
+        .bio-info h2 {
+          margin-top: 0;
+          margin-left: 0.15rem;
+          margin: 0 0.15rem 1rem 0.15rem;
+          font-size: 0.9rem;
+          color: #fff;
+          font-weight: 300;
+          text-transform: uppercase;
+          letter-spacing: 2px;
+        }
 
-										.bio-image {
-											background-size: cover;
-											background-position: center;
-											height: 100%;
-										}
+        .bio-info p {
+          color: #d7a08b;
+          line-height: 2;
+        }
 
-										@media (max-width: 768px) { 
-										.bio-grid {
-												grid-template-columns: 1fr;
-												height: 100%;
-											}
+        .bio-image {
+          background-size: cover;
+          background-position: center;
+          height: 100%;
+        }
 
-										.bio-image {
-											height: 450px;
-											background-position: center;
-										}
-										}
+        @media (max-width: 768px) {
+          .bio-grid {
+            grid-template-columns: 1fr;
+            height: 100%;
+          }
 
-										@media (prefers-color-scheme: dark) { 
-											.bio-grid {
-													background-color: #15102f;
-												}
-										}
-    
-							`}</style>
-						</div>
-				)
-		}
+          .bio-image {
+            height: 450px;
+            background-position: center;
+          }
+        }
 
-}
+        @media (prefers-color-scheme: dark) {
+          .bio-grid {
+            background-color: #15102f;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default Bio;
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,10 @@ export default function Home() {
 				<Bio 
 					userName="Gabe Sousa"
 					userImage="/images/gabe.jpg"
-					userTitle="Web Developer"
+					userTitle="Front-End Developer"
 					userBio="Howdy! I'm a developer with experience in all sorts of technologies and frameworks like React, Vue.js, and even good ol' WordPress. I've got a knack for delivering top-notch work, and I've engineered all sorts of cool stuff, like SPAs, custom landing pages, email templates, and even some sweet web animations. Let's build something awesome together!"
 				/>
+
       </main>
   )
 }
