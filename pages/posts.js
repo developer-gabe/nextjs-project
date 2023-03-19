3330778ccd7ccde195e4542503f0ff064a964c48
developer-gabe <gsousa@ampagency.com>
2023-03-19 17:22:22 -0600

    Add "No Results" when results are blank

--- pages/posts.js
+++ pages/posts.js
@@ -14,25 +14,26 @@ export async function getStaticProps() {
 export default function Posts ({ allPostsData }) {
   const [searchQuery, setSearchQuery] = useState('');
 
+  const filteredPosts = allPostsData.filter(({ title, tags }) => 
+    title.toLowerCase().includes(searchQuery.toLowerCase()) || 
+    tags.some(tag => tag.toLowerCase().includes(searchQuery.toLowerCase()))
+  );
+
   return (
     <main className='Posts'>
       <section className='blog-list__section'>
-			<h2>Search:</h2>
+        <h2>Search:</h2>
         <input
           type="text"
           value={searchQuery}
           onChange={(event) => setSearchQuery(event.target.value)}
           placeholder="Find..."
-					className='search-input'
+          className='search-input'
         />
-				<h2>Posts:</h2>
-        <ul className="blog-list">
-          {allPostsData
-            .filter(({ title, tags }) => 
-              title.toLowerCase().includes(searchQuery.toLowerCase()) || 
-              tags.some(tag => tag.toLowerCase().includes(searchQuery.toLowerCase()))
-            )
-            .map(({ id, date, title }) => (
+        <h2>Posts:</h2>
+        {filteredPosts.length > 0 ? (
+          <ul className="blog-list">
+            {filteredPosts.map(({ id, date, title }) => (
               <li key={id} className="blog-list__item">
                 <Link href={`/posts/${id}`}>{title}</Link>
                 <p className='blog-post__date'>
@@ -44,7 +45,10 @@ export default function Posts ({ allPostsData }) {
                 </p>
               </li>
             ))}
-        </ul>
+          </ul>
+        ) : (
+          <p>Nothing here...</p>
+        )}
       </section>
     </main>
   )
