fab5b72e4c1930394f5eae7184d27334403e4e87
developer-gabe <gsousa@ampagency.com>
2023-03-19 13:08:32 -0600

    Change body colors, update head

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import App from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 import Header from '../components/header'
 import '../styles/blog-post.css'
@@ -7,6 +8,13 @@ import 'prism-themes/themes/prism-shades-of-purple.css';
 
 function MyApp({ Component, pageProps }) {
 	return <>
+					<Head>
+						<title>Gabe Sousa</title>
+						<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+						<meta name="theme-color" content="#fdced0" media="(prefers-color-scheme: light)"></meta>
+						<meta name="theme-color" content="#14112e" media="(prefers-color-scheme: dark)"></meta>
+						<link rel="icon" href="/favicon.ico" />
+					</Head>
 					<Header /> 
 					<Component {...pageProps} />
 					<footer>GabeSousa.com 2023</footer>
